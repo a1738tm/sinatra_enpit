2f2dcb456210be27ce60576eb815a106968a8fff
murakami <a1738tm@aiit.ac.jp>
2017-07-12 19:56:11 +0900

    Add some urls

--- hello.rb
+++ hello.rb
@@ -3,3 +3,12 @@ require 'sinatra'
 get '/' do
   "Hello World!"
 end
+
+get '/hello/:name' do |n|
+#  "Hello #{params['name']}!"
+  "Hello #{n}!"
+end
+
+get '/say/*/to/*' do
+  "#{params['splat'][0]} #{params['splat'][1]}!"
+end
